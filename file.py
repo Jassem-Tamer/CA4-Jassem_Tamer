c891b30559a5b0b286e0ebf720b439855f3a84d4
Jassem Alhaj tamer <jeo@Jassems-MacBook-Pro.local>
2017-05-07 15:29:14 +0100

    adding two new functions

--- file.py
+++ file.py
@@ -20,4 +20,29 @@ class Commit(object):
 				continue
 		
 			
+	def get_number_of_updated_commits(self,data):
+		count = 0
+		for line in data:
+			if line.startswith("Updated"):
+				word = line.split()
+				print word[0:2]
+				count = count + 1
+		return count
+		
+	def get_authors(self,data):
+		sep = 72*'-'
+		authors = []
+		index = 0
+		
+		while index < len(data):
+			try:
+				index = data.index(sep, index + 1)
+				details = data[index + 1].split('|')
+				author = details[1].strip()
+				if author not in authors:
+					authors.append(author)
+			except IndexError:
+				break
+		return authors 
+		
 	
